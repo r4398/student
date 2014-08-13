0400a7f35a22ce01deacb93f9cb2ef4fb234af68
Yury Reshetnikov (on cctv2) <yura.reshetnikov@portal59.ru>
2014-08-13 17:03:40 +0600

    _

---
+++ tasks/reshu/JsWikiMarkup/markup.js
@@ -0,0 +1,12 @@
+(function(jQuery) {
+    if (typeof(jQuery.reshu) === "undefined") {
+        jQuery.reshu = {}
+    }
+
+    var plugin
+
+    plugin = jQuery.reshu.markup = function(src_base_path) {
+	console.log('форель')
+    }
+
+})(jQuery)
