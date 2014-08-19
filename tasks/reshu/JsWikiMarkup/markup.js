c2b05286a62b8cdce728936882892d2dc3ece6a7
Yury Reshetnikov (on cctv2) <yura.reshetnikov@portal59.ru>
2014-08-19 16:25:25 +0600

    _

--- tasks/reshu/JsWikiMarkup/markup.js
+++ tasks/reshu/JsWikiMarkup/markup.js
@@ -9,6 +9,24 @@
 	var url = src_base_path + location.search.replace(/^\?s=/, '')
 	$.ajax({type: 'GET', url: url, success: function(text) {
 	    $(document.body).append($('<pre/>').text(text))
+	    var pos
+	    while((pos = text.indexOf('\n')) != -1) {
+		var line = text.substring(0, pos)
+		text = text.substring(pos + 1)
+		if(line[0] == '!') {
+		    var level = 0
+		    while(line[++level] == '!') ;
+		    if(line[level] == ' ') {
+			var start = level
+			while(line[++start] == ' ') ;
+			if(start < line.length) {
+			    $(document.body).append($('<h'+level+'/>').text(line.substr(start)))
+			    continue
+			}
+		    }
+		}
+		$(document.body).append($('<p/>').text(line))
+	    }
 	}})
     }
 
