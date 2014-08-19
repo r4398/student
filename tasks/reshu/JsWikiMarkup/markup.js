3799d6f446177d82a37b41b7c14ca1f2e5ee1674
Yury Reshetnikov (on cctv2) <yura.reshetnikov@portal59.ru>
2014-08-19 16:07:16 +0600

    _

--- tasks/reshu/JsWikiMarkup/markup.js
+++ tasks/reshu/JsWikiMarkup/markup.js
@@ -6,12 +6,8 @@
     var plugin
 
     plugin = jQuery.reshu.markup = function(src_base_path) {
-	console.log('форель')
-	console.log(src_base_path)
 	var url = src_base_path + location.search.replace(/^\?s=/, '')
-	console.log(url)
 	$.ajax({type: 'GET', url: url, success: function(text) {
-	    console.log(text)
 	    $(document.body).append($('<pre/>').text(text))
 	}})
     }
