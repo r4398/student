f6334d85490d5f29168681d670a6890796daeaca
Yury Reshetnikov (on cctv2) <yura.reshetnikov@portal59.ru>
2014-08-20 18:10:44 +0600

    _

--- tasks/reshu/JsWikiMarkup/markup.js
+++ tasks/reshu/JsWikiMarkup/markup.js
@@ -5,10 +5,30 @@
 
     var plugin
 
+    function Context() {
+	return {
+	    lists: 0,
+	    paragraph: function(text) {
+		$(document.body).append($('<p/>').text(text))
+	    },
+	    header: function(level, text) {
+		$(document.body).append($('<h'+level+'/>').text(text))
+	    },
+	    list: function(list, text) {
+		++this.lists
+		$(document.body).append($('<ul/>').append($('<li/>').text(text)))
+	    },
+	    log: function() {
+		console.log(this)
+	    },
+	}
+    }
+
     plugin = jQuery.reshu.markup = function(src_base_path) {
 	var url = src_base_path + location.search.replace(/^\?s=/, '')
 	$.ajax({type: 'GET', url: url, success: function(text) {
 	    $(document.body).append($('<pre/>').text(text))
+	    var ctx = Context()
 	    var pos
 	    while((pos = text.indexOf('\n')) != -1) {
 		var line = text.substring(0, pos)
@@ -20,13 +40,26 @@
 			var start = level
 			while(line[++start] == ' ') ;
 			if(start < line.length) {
-			    $(document.body).append($('<h'+level+'/>').text(line.substr(start)))
+			    ctx.header(level, line.substr(start))
 			    continue
 			}
 		    }
 		}
-		$(document.body).append($('<p/>').text(line))
+		else if(line[0] == '*' || line[0] == '-') {
+		    var level = 0
+		    while(line[++level] == '*' || line[level] == '-') ;
+		    if(line[level] == ' ') {
+			var start = level
+			while(line[++start] == ' ') ;
+			if(start < line.length) {
+			    ctx.list(line.substr(0, level), line.substr(start))
+			    continue
+			}
+		    }
+		}
+		ctx.paragraph(line)
 	    }
+	    ctx.log()
 	}})
     }
 
