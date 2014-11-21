a3875c36cea6cc5dcf8d14dc9634ed2b60962615
Yury Reshetnikov (reshu-big) <reshu@mail.ru>
2014-11-22 02:47:47 +0500

    _

--- tasks/reshu/AutoPage/site_perl/AutoPage/Application.pm
+++ tasks/reshu/AutoPage/site_perl/AutoPage/Application.pm
@@ -3,6 +3,7 @@ package AutoPage::Application;
 use strict;
 use warnings;
 use bytes;
+use Reshu::Utils;
 
 our $first_request = 1;
 our $start_time; BEGIN { $start_time = time; }
@@ -12,13 +13,18 @@ sub run {
     eval "require $pkg; $pkg\->run;";
     if(my $err = $@) {
 	warn $err;
+	xopen((my $listen_sock), "<&=", &AutoPage::FCGI::FCGI_LISTENSOCK_FILENO);
+	$listen_sock->blocking(1);
 	require FCGI;
 	my $request = FCGI::Request();
-	while($request->Accept() >= 0) {
+	my $ret;
+	while(($ret = $request->Accept()) >= 0) {
 	    print "Content-type: text/html\r\n\r\nerror\r\n";
+	    print 'fast:', d $request->IsFastCGI;
 	    print "<br />\r\n";
 	    print `id`;
 	}
+	warn eval dw qw($ret $request);
     }
 }
 
