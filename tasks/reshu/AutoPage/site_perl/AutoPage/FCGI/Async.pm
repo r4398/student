8e0b5a6c3623f61c3739fa234f553fd7748478a2
Yury Reshetnikov (reshu-big) <reshu@mail.ru>
2014-12-13 08:53:34 +0500

    _

--- tasks/reshu/AutoPage/site_perl/AutoPage/FCGI/Async.pm
+++ tasks/reshu/AutoPage/site_perl/AutoPage/FCGI/Async.pm
@@ -3,9 +3,10 @@ package AutoPage::FCGI::Async;
 use strict;
 use warnings;
 use bytes;
-require AnyEvent;
+use AnyEvent ();
 require AnyEvent::Handle;
 use Reshu::Utils;
+use PageGen::Utils qw(unescape_uri);
 use base qw(AutoPage::FCGI);
 
 1;
@@ -13,30 +14,34 @@ use base qw(AutoPage::FCGI);
 sub accept {
     my $self = shift;
     my $session = shift;
-    my $fail = shift || sub { AE::log error => $msg; };
+    my $fail = shift || sub { AE::log error => shift; };
     $self->{sock_aw} = AE::io $self->{sock}, 0, sub {
 	while(my $peer = accept my $fh, $self->{sock}) {
 	    my($service, $host) = unpack_sockaddr $peer;
 	    my $reading;
-	    $r->{sock_aw} = AnyEvent::Handle
+	    my $r;
+	    my $sock_aw; $sock_aw = new AnyEvent::Handle
 		fh => $fh,
 		on_error => sub {
 		    my($hdl, $fatal, $msg) = @_;
 		    $fail->($msg);
 		    $hdl->destroy;
+		    undef $sock_aw;
 		    delete $r->{sock_aw};
 		},
 		on_eof => sub {
 		    my($hdl) = @_;
 		    if($reading || $hdl->{rbuf} ne '') { $fail->('Unexpected end of stream'); }
 		    $hdl->destroy;
+		    undef $sock_aw;
 		    delete $r->{sock_aw};
 		};
 	    my $error = sub {
 		my $msg = shift;
 		$fail->($msg);
-		if($r->{sock_aw}) {
-		    $r->{sock_aw}->destroy;
+		if($sock_aw) {
+		    $sock_aw->destroy;
+		    undef $sock_aw;
 		    delete $r->{sock_aw};
 		}
 		return;
@@ -46,7 +51,7 @@ sub accept {
 		my $cb = shift;
 		return $error->('Algorithm error') if $reading;
 		$reading = 1;
-		$r->{sock_aw}->push_read(chunk => $size, sub {
+		$sock_aw->push_read(chunk => $size, sub {
 		    my $data = $_[1];
 		    $reading = 0;
 		    $cb->($data);
@@ -55,7 +60,7 @@ sub accept {
 	    my $read_packet = sub {
 		my $cb = shift;
 		$read->(&AutoPage::FCGI::FCGI_HEADER_LEN, sub {
-		    my($ver, $type, $req_id, $size, $padding) = unpack('CCnnC', $buf);
+		    my($ver, $type, $req_id, $size, $padding) = unpack('CCnnC', shift);
 		    return $error->('Unknow FCGI protocol version:'.$ver) if $ver != &AutoPage::FCGI::FCGI_VERSION_1;
 		    my $read_packet_data = sub {
 			my $data = shift;
@@ -72,7 +77,7 @@ sub accept {
 		my($role, $flags) = unpack("nC", $data);
 		return $error->('*') if $type != &AutoPage::FCGI::FCGI_BEGIN_REQUEST;
 		return $error->('*') if $role != &AutoPage::FCGI::FCGI_RESPONDER;
-		AutoPage::FCGI::Async::Request->run($req_id, $read_packet, $error, $session);
+		$r = AutoPage::FCGI::Async::Request->run($sock_aw, $req_id, $read_packet, $error, $session);
 	    });
 	}
    };
@@ -91,9 +96,10 @@ use base qw(AutoPage::FCGI);
 sub run {
     my $class = shift;
     my $r = bless { stdout_buf => '' }, ref($class) || $class;
+    $r->{sock_aw} = shift;
     $r->{id} = shift;
     $r->read_params(@_);
-    return;
+    return $r;
 }
 
 sub read_params {
@@ -101,7 +107,7 @@ sub read_params {
     my($read_packet, $error, $session) = @_;
     my $data = '';
     my $wait_size = 0;
-    my $read_params_pice = sub {
+    my $read_params_pice; $read_params_pice = sub {
 	$read_packet->(sub {
 	    my($type, $req_id, $data_pice) = @_;
 	    return $error->('*') if $type != &AutoPage::FCGI::FCGI_PARAMS;
@@ -148,12 +154,12 @@ sub read_stdin {
 	    foreach my $p (@d) {
 		my @p = split(/=/, $p, 2);
 		push @{$r->{post}}, map &unescape_uri($_), @p;
-		if(@p == 1) { push @$post, undef; }
+		if(@p == 1) { push @{$r->{post}}, undef; }
 	    }
 	};
     }
     else { $parse = sub {}; }
-    my $read = sub {
+    my $read; $read = sub {
 	$read_packet->(sub {
 	    my($type, $req_id, $data_pice) = @_;
 	    return $error->('*') if $type != &AutoPage::FCGI::FCGI_STDIN;
@@ -180,7 +186,7 @@ sub write_packet {
     my $req_id = shift;
     my $data = shift;
     my $padding = (8 - length($data) % 8) % 8;
-    my $header = pack('CCnnCC', FCGI_VERSION_1, $type, $req_id, length($data), $padding, 0);
+    my $header = pack('CCnnCC', &AutoPage::FCGI::FCGI_VERSION_1, $type, $req_id, length($data), $padding, 0);
     $r->{sock_aw}->push_write($header . $data . ($padding ? ("\0" x $padding) : ''));
 }
 
